7113a2bb4ea1de37b7bda55460f343f471138328
Fedor Garbuzov <fedorgarbuzov@yandex.ru>
2020-10-27 16:16:02 +0300

    new fields added

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -21,5 +21,77 @@ data class Post(
     val isPinned: Boolean = true,
     val markedAsAds: Boolean = true,
     val isFavorite: Boolean = true,
-    val postponedId: Int = 0
-)
+    val postponedId: Int = 0,
+    val postSource: PostSource = PostSource(),
+    val geo: Geo = Geo(),
+    val copyHistory: Array<Post>? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Post
+
+        if (id != other.id) return false
+        if (ownerId != other.ownerId) return false
+        if (fromId != other.fromId) return false
+        if (createdBy != other.createdBy) return false
+        if (date != other.date) return false
+        if (text != other.text) return false
+        if (replyOwnerId != other.replyOwnerId) return false
+        if (replyPostId != other.replyPostId) return false
+        if (friendsOnly != other.friendsOnly) return false
+        if (comments != other.comments) return false
+        if (copyright != other.copyright) return false
+        if (likes != other.likes) return false
+        if (reposts != other.reposts) return false
+        if (views != other.views) return false
+        if (postType != other.postType) return false
+        if (signerId != other.signerId) return false
+        if (canPin != other.canPin) return false
+        if (canDelete != other.canDelete) return false
+        if (canEdit != other.canEdit) return false
+        if (isPinned != other.isPinned) return false
+        if (markedAsAds != other.markedAsAds) return false
+        if (isFavorite != other.isFavorite) return false
+        if (postponedId != other.postponedId) return false
+        if (postSource != other.postSource) return false
+        if (geo != other.geo) return false
+        if (copyHistory != null) {
+            if (other.copyHistory == null) return false
+            if (!copyHistory.contentEquals(other.copyHistory)) return false
+        } else if (other.copyHistory != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + ownerId
+        result = 31 * result + fromId
+        result = 31 * result + createdBy
+        result = 31 * result + date
+        result = 31 * result + text.hashCode()
+        result = 31 * result + replyOwnerId
+        result = 31 * result + replyPostId
+        result = 31 * result + friendsOnly.hashCode()
+        result = 31 * result + comments.hashCode()
+        result = 31 * result + copyright.hashCode()
+        result = 31 * result + likes.hashCode()
+        result = 31 * result + reposts.hashCode()
+        result = 31 * result + views.hashCode()
+        result = 31 * result + postType.hashCode()
+        result = 31 * result + signerId
+        result = 31 * result + canPin.hashCode()
+        result = 31 * result + canDelete.hashCode()
+        result = 31 * result + canEdit.hashCode()
+        result = 31 * result + isPinned.hashCode()
+        result = 31 * result + markedAsAds.hashCode()
+        result = 31 * result + isFavorite.hashCode()
+        result = 31 * result + postponedId
+        result = 31 * result + postSource.hashCode()
+        result = 31 * result + geo.hashCode()
+        result = 31 * result + (copyHistory?.contentHashCode() ?: 0)
+        return result
+    }
+}
