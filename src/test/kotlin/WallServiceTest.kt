115f77f0c90ba0b0bc5c5dd1959f2d5884aebeae
Fedor Garbuzov <fedorgarbuzov@yandex.ru>
2020-10-30 14:35:31 +0300

    comments function tests added

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,3 +1,4 @@
+import org.junit.Assert
 import org.junit.Test
 
 class WallServiceTest {
@@ -5,5 +6,34 @@ class WallServiceTest {
     @Test
     fun add() {
         WallService.add(Post())
+    }
+
+    @Test
+    fun updateTrue() {
+        val post = Post()
+        WallService.add(post)
+
+        val result = WallService.update(post)
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun updateFalse() {
+
+        Assert.assertFalse(WallService.update(Post()))
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        WallService.createComment(Comment(postId = 1))
+    }
+
+    @Test
+    fun shouldNotThrow() {
+        val post = Post()
+        WallService.add(post)
+        val comment = Comment(postId = 0)
+        WallService.createComment(comment)
     }
 }
