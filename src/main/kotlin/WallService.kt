50c3434ee208e6d70b877f257477fdcf5d052d5d
Fedor Garbuzov <fedorgarbuzov@yandex.ru>
2020-11-02 12:31:57 +0300

    comment report function added

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,10 +1,14 @@
 import java.lang.RuntimeException
+
 class PostNotFoundException(message: String) : RuntimeException(message)
+class CommentNotFoundException(message: String) : RuntimeException(message)
+class ReasonNotFoundException(message: String) : RuntimeException(message)
 
 object WallService {
     private var id = 0
     var posts = emptyArray<Post>()
     var comments = emptyArray<Comment>()
+    var reports = emptyArray<CommentReport>()
 
     fun add(post: Post): Post {
         posts += post.copy(id = id++)
@@ -23,5 +27,24 @@ object WallService {
         val old = posts.getOrElse(indexOfPost) { throw PostNotFoundException("Пост с указанным id '${comment.postId}' не найден") }
         comments += comment
         posts[indexOfPost] = old.copy(comments = Comments(old.comments.count + 1))
+    }
+
+    fun reportComment(commentReport: CommentReport) {
+        val target = comments.find { it.id == commentReport.commentId && it.fromId == commentReport.ownerId }
+        if (target == null) throw CommentNotFoundException("Комментарий с заданными параметрами не найден") else reports += commentReport
+        val old = reports.last()
+        val indexOfReport = reports.lastIndex
+        val reportReason = when (commentReport.reason) {
+            0 -> "cпам"
+            1 -> "детская порнография"
+            2 -> "экстремизм"
+            3 -> "насилие"
+            4 -> "пропаганда наркотиков"
+            5 -> "материал для взрослых"
+            6 -> "оскорбление"
+            7 -> "призывы к суициду"
+            else -> throw ReasonNotFoundException("Неверно указана причина жалобы")
+        }
+        reports[indexOfReport] = old.copy(reason = reportReason)
     }
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -21,19 +21,42 @@ class WallServiceTest {
     @Test
     fun updateFalse() {
 
-        Assert.assertFalse(WallService.update(Post()))
+        Assert.assertFalse(WallService.update(Post(id = 2)))
     }
 
     @Test(expected = PostNotFoundException::class)
-    fun shouldThrow() {
+    fun shouldThrowPostNotFoundException() {
         WallService.createComment(Comment(postId = 1))
     }
 
     @Test
-    fun shouldNotThrow() {
+    fun shouldNotThrowPostNotFoundException() {
         val post = Post()
         WallService.add(post)
         val comment = Comment(postId = 0)
         WallService.createComment(comment)
+    }
+
+    @Test(expected = CommentNotFoundException::class)
+    fun throwCommentNotFoundException() {
+        WallService.reportComment(CommentReport(0, 2, 3))
+    }
+
+    @Test
+    fun shouldNotThrowCommentNotFoundException() {
+        val post = Post()
+        WallService.add(post)
+        val comment = Comment(postId = 0)
+        WallService.createComment(comment)
+        WallService.reportComment(CommentReport(0, 0, 3))
+    }
+
+    @Test(expected = ReasonNotFoundException::class)
+    fun throwReasonNotFoundException() {
+        val post = Post()
+        WallService.add(post)
+        val comment = Comment(postId = 0)
+        WallService.createComment(comment)
+        WallService.reportComment(CommentReport(0, 0, 8))
     }
 }
